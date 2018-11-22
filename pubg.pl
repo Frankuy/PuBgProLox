f683960675c5e60a13741848694e30632c9ccfa0
Dandi99 <d.agusmaulana99@gmail.com>
2018-11-22 19:07:34 +0700

    Update pubg.pl

--- pubg.pl
+++ pubg.pl
@@ -6,7 +6,6 @@ dynamic(playerArmor/1).
 dynamic(playerInventory/2).
 dynamic(playerEquip/2).
 dynamic(luasmap/2).
-
 weapon(akm, 50, 0).
 weapon(ump, 35, 0).
 weapon(pistol, 10, 0).
@@ -18,7 +17,14 @@ medicine(firstaid, 70).
 medicine(bandage, 10).
 
 /* RULES */
-start :- write('Selamat datang di Pulau Champs.'), nl, 
+start :- write(' _____    _    _   ____     _____ '),nl,
+		 write('|  __ \\  | |  | | |  _ \\   / ____|'),nl,
+		 write('| |__) | | |  | | | |_) | | |  __'),nl,
+		 write('|  ___/  | |  | | |  _ <  | | |_ |'),nl,
+		 write('| |      | |__| | | |_) | | |__| |'),nl,
+		 write('|_|       \\____/  |____/   \\_____|  Jagonya Ayam'),nl,   
+		 write('                                     '),nl,
+		 write('Selamat datang di Pulau Champs.'), nl, 
 		 write('Bersenang-senanglah di pulau ini!. Tapi ingat only one can survive and get the title "Jagonya Ayam"'), nl, 
 		 write('Jika kamu menang, Kamu juga akan mendapatkan persediaan sosis selama 1 tahun penuh. So, let\'s the battle royale begin!'), nl, nl,
 		 write('Perintah yang tersedia : '),nl,
@@ -45,28 +51,44 @@ start :- write('Selamat datang di Pulau Champs.'), nl,
 		 write('    X = Inaccesible'),
 		 random(0,10,X),
 		 random(0,10,Y),
-		 assertz(playerPosition(X,Y)),
-		 assertz(weaponPosition(akm, X+1, Y+1)),
-		 assertz(weaponPosition(clurit, X, Y-1)),
-		 assertz(weaponPosition(gearsepeda, X, Y+1)),
+		 assertz(playerPosition(2,2)),
+		 assertz(weaponPosition(akm, 1, 1)),
+		 assertz(weaponPosition(clurit, 2, 3)),
+		 assertz(weaponPosition(gearsepeda, 3, 3)),
 		 assertz(playerHealth(100)),
 		 assertz(playerArmor(0)),
 		 assertz(playerInventory(0,5)),
 		 assertz(playerEquip(none, 0)),
 		 assertz(luasmap(10, 10)).
 
-help :- write('    start.'), nl,
-		 write('    help.'), nl,
-		 write('    quit.'), nl,
-		 write('    NSEW.'), nl,
-		 write('    map.'),nl,
-		 write('    take(object).'), nl,
-		 write('    drop(object).'), nl,
-		 write('    use(object).'), nl,
-		 write('    attack.'),nl,
-		 write('    status.'),nl,
-		 write('    save(filename).'),nl,
-		 write('    load(filename).').
+help :-     nl, print('      =================================================================================='),
+			nl, print('     |Available commands to help you survive the game and win the title  "Jagonya Ayam"|'),
+			nl, print('     |=================================================================================|'),
+			nl, print('     |start.            | Start the game.                                              |'),
+			nl, print('     |help.             | Get some help to look over commands available.               |'),
+			nl, print('     |quit.             | Farewell, quit the game.                                     |'),
+			nl, print('     |look.             | Look around you.                                             |'),
+			nl, print('     |map.              | Open map and see where on Earth are you.                     |'),
+			nl, print('     |N. E. S. W.       | Move to the North, East, South, or West.                     |'),
+			nl, print('     |take(Object).     | Pick up an object.                                           |'),
+			nl, print('     |drop(Object).     | Drop an object.                                              |'),
+			nl, print('     |use(Object).      | Use an object from your inventory.                           |'),
+			nl, print('     |attack.           | Attack the enemy that crosses your path.                     |'),
+			nl, print('     |status.           | Show your status.                                            |'),
+			nl, print('     |save(FileName).   | Save your game.                                              |'),
+			nl, print('     |load(FileName).   | Load your previously saved game.                             |'),
+			nl, print('     |=================================================================================|'),
+			nl, print('     |                            Information in the map                               |'),
+			nl, print('     |=================================================================================|'),
+			nl, print('     |         W        | Weapon                                                       |'),
+			nl, print('     |         A        | Armor                                                        |'),
+			nl, print('     |         M        | Medicine                                                     |'),
+			nl, print('     |         O        | Ammo                                                         |'),
+			nl, print('     |         P        | Player                                                       |'),
+			nl, print('     |         E        | Enemy                                                        |'),
+			nl, print('     |         -        | Accessible                                                   |'),
+			nl, print('     |         X        | Inaccessible                                                 |'),
+			nl, print('      ==================================================================================').
 
 quit :- halt.
 
@@ -81,29 +103,22 @@ status :- playerHealth(Health), playerArmor(Armor), playerEquip(Equipment, _), p
 			write('Weapon : '), write(Equipment), nl, 
 			Jumlah = 0, write('Your inventory is empty!').
 			
-/* 
-attack :- playerEquip(X, Y).
-*/
-
-/* look :- playerPosition(X,Y),
-		weaponPosition(J,A,B),
-		cetak(X-1,A,Y+1,B,'W', J),
-		weaponPosition(J,A,B),
-		cetak(X,A,Y+1,B,'W', J),
-		weaponPosition(J,A,B),
-		cetak(X+1,A,Y+1,B,'W', J),nl,
-		weaponPosition(J,A,B),
-		cetak(X-1,A,Y,B,'W', J),
-		weaponPosition(J,A,B),
-		cetak(X,A,Y,B,'W', J),
-		weaponPosition(J,A,B),
-		cetak(X+1,A,Y,B,'W', J),nl,
-		weaponPosition(J,A,B),
-		cetak(X-1,A,Y-1,B,'W', J),
-		weaponPosition(J,A,B),
-		cetak(X,A,Y-1,B,'W', J),
-		weaponPosition(J,A,B),
-		cetak(X+1,A,Y-1,B,'W', J). */
+look :- playerPosition(X,Y),
+		A is X-1, B is Y-1, cek(A,B),
+		C is X, D is Y-1,cek(C,D),
+		E is X+1, F is Y-1,cek(E,F),nl,
+		G is X-1, H is Y,cek(G,H),
+		I is X, J is Y,cek(I,J),
+		K is X+1, L is Y,cek(K,L),nl,
+		M is X-1, N is Y+1,cek(M,N),
+		O is X, P is Y+1,cek(O,P),
+		Q is X+1, R is Y+1,cek(Q,R).
 		
-/* cetak(X,XBenda,Y,YBenda, Benda) :- XBenda = X, YBenda = Y, write(Benda), !.
-cetak(X,XBenda,Y,YBenda, Benda) :- write('-'). */
+/*cetak(X,XBenda,Y,YBenda, Benda, NamaBenda) :- XBenda = X, YBenda = Y, write(Benda), retract(weaponPosition(NamaBenda, XBenda, YBenda)),assertz(weaponPosition(NamaBenda, XBenda, YBenda)), !.
+cetak(X,XBenda,Y,YBenda, Benda, NamaBenda) :- write('-').
+cariObjek(X,Y,Simbol):- */
+
+attack :- playerHealth(X), Z is X-20, retract(playerHealth(X)), asserta(playerHealth(Z)). 
+
+cek(X,Y):-weaponPosition(_,X,Y),write('W'),!.
+cek(X,Y):-write('-').
